092a32c4e8974db412650bb05f7820f908c72257
Brukl1n <enot.east@gmail.com>
2022-12-19 21:58:48 +0300

    Completed sith homework

---
+++ homework_6/task_41/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] CreateArray(int arraySize) {
+    int[] array = new int[arraySize];
+    Console.WriteLine();
+    return array;
+}
+
+int[] ArrayFilling(int[] emptyArray) {
+    Console.WriteLine("Поочерёдно вводите любые положительные или отрицательные числа: ");
+    for(int i = 0; i < emptyArray.Length; i++) emptyArray[i] = int.Parse(Console.ReadLine()!);
+    return emptyArray;
+}
+
+void ResultOutput(int[] filledArray) {
+    int count = 0;
+    for(int i = 0; i < filledArray.Length; i++) if(filledArray[i] > 0) count += 1;
+    Console.WriteLine($"Чисел больше 0 было введено: {count}.");
+}
+
+Console.Clear();
+
+Console.WriteLine("Привет! Эта программа принимает на ввод числа в количестве M и выводит сколько было введено чисел больше 0.");
+Console.WriteLine();
+
+Console.Write("Ввведите количество чисел (M), которые вы будете затем вводить: ");
+int M = int.Parse(Console.ReadLine()!);
+
+ResultOutput(ArrayFilling(CreateArray(M)));
